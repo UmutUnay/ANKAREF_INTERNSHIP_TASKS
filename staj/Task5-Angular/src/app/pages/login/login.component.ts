185e245dd9927ef62ca0b2af724d3ae8950f4bfe
Umut Ünay <umut.unay@metu.edu.tr>
2024-07-19 10:40:28 +0300

    Added Frontend
    Finally

---
+++ staj/Task5-Angular/src/app/pages/login/login.component.ts
@@ -0,0 +1,136 @@
+import {Component, ElementRef, HostListener, OnDestroy, OnInit, Renderer2} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {Router, RouterModule} from "@angular/router";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  isOpened: boolean = false;
+  private closeButtonListenerFn: Function | null = null;
+  loginObject: Login;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2, private http: HttpClient, private router: Router) {
+    this.loginObject = new Login();
+  }
+
+  ngOnInit(): void {
+    this.attachCloseButtonListener();
+  }
+
+
+  openModal(): void {
+    const modal = document.querySelector('.modal');
+    if (modal) {
+      modal.classList.add('is-open');
+      document.body.style.overflow = 'hidden';
+    }
+  }
+
+  closeModal(): void {
+    const modal = document.querySelector('.modal');
+    if (modal) {
+      modal.classList.remove('is-open');
+      document.body.style.overflow = 'initial';
+      this.isOpened = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.closeButtonListenerFn) {
+      this.closeButtonListenerFn();
+    }
+  }
+
+  onLogin(): void {
+    alert("Trying Login")
+    this.http.post('http://localhost:8080/auth/generateToken', this.loginObject).subscribe((response:any) => {
+      if(response.result)
+      {
+        alert("Login Successful")
+        this.router.navigate(['/dashboard']);
+      }
+      else
+      {
+        alert("Login Failed")
+      }
+    })
+  }
+
+  // Listeners
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(): void {
+    if (window.scrollY > window.innerHeight / 3 && !this.isOpened) {
+      this.isOpened = true;
+      this.openModal();
+    }
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  handleKeyboardEventEscape (event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      this.closeModal();
+    }
+  }
+
+  @HostListener('document:keydown.enter', ['$event'])
+  handleKeyboardEventEnter(event: KeyboardEvent): void {
+    if (event.key === 'Enter') {
+      this.onLogin();
+    }
+  }
+
+  attachCloseButtonListener(): void {
+    const closeButton = this.elRef.nativeElement.querySelector('.close-button');
+    if (closeButton) {
+      this.closeButtonListenerFn = this.renderer.listen(closeButton, 'click', () => {
+        this.closeModal();
+      });
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleOpenLogInButton(event: MouseEvent): void {
+    const modal = document.querySelector('.modal-button');
+    if (event.target === modal) {
+      this.openModal();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleLogInButton(event: MouseEvent): void {
+    const modal = document.querySelector('.modal-buttons-input-button');
+    if (event.target === modal) {
+      this.onLogin();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleRegisterButton(event: MouseEvent): void {
+    const modal = document.querySelector('.modal-buttons-a');
+    if (event.target === modal) {
+      this.router.navigate(['/register']);
+    }
+  }
+
+}
+
+export class Login
+{
+  email: string;
+  password: string;
+  constructor()
+  {
+    this.email = "";
+    this.password = "";
+  }
+}
---
+++ staj/Task5-Angular/src/app/pages/register/register.component.ts
@@ -0,0 +1,152 @@
+import {Component, ElementRef, HostListener, OnDestroy, OnInit, Renderer2} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import {Router, RouterModule} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+    imports: [
+        FormsModule,
+      RouterModule
+    ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss'
+})
+export class RegisterComponent implements OnInit, OnDestroy
+{
+  isOpened: boolean = false;
+  private closeButtonListenerFn: Function | null = null;
+  registerObject: Register;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2, private http: HttpClient, private router: Router)
+  {
+    this.registerObject = new Register();
+  }
+
+  ngOnInit(): void
+  {
+    this.attachCloseButtonListener();
+  }
+
+  ngOnDestroy(): void
+  {
+    if (this.closeButtonListenerFn)
+    {
+      this.closeButtonListenerFn();
+    }
+  }
+
+  openModal(): void
+  {
+    const modal = document.querySelector('.modal');
+    if (modal)
+    {
+      modal.classList.add('is-open');
+      document.body.style.overflow = 'hidden';
+    }
+  }
+
+  closeModal(): void
+  {
+    const modal = document.querySelector('.modal');
+    if (modal)
+    {
+      modal.classList.remove('is-open');
+      document.body.style.overflow = 'initial';
+      this.isOpened = false;
+    }
+  }
+
+  onRegister(): void
+  {
+    this.http.post('http://localhost:8080/auth/register', this.registerObject).subscribe((response: any) =>
+    {
+      if (response.success)
+      {
+        this.router.navigate(['/login']);
+      }
+      else
+      {
+        alert("Registration Failed");
+      }
+    });
+  }
+
+
+  // Listeners
+  @HostListener('document:click', ['$event'])
+  handleOpenRegisterButton(event: MouseEvent): void // Somehow not working
+  {
+    const modal = document.querySelector('.modal-button');
+    if (event.target === modal)
+    {
+      this.openModal();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleRegisterButton(event: MouseEvent): void
+  {
+    const modal = document.querySelector('.modal-buttons-input-button');
+    if (event.target === modal)
+    {
+      this.onRegister();
+    }
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  handleEscapeKey(event: KeyboardEvent): void
+  {
+    if (event.key === 'Escape')
+    {
+      this.closeModal();
+    }
+  }
+
+  @HostListener('document:keydown.enter', ['$event'])
+  handleEnterKey(event: KeyboardEvent): void
+  {
+    if (event.key === 'Enter')
+    {
+      this.onRegister();
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(): void {
+    if (window.scrollY > window.innerHeight / 3 && !this.isOpened) {
+      this.isOpened = true;
+      this.openModal();
+    }
+  }
+
+  attachCloseButtonListener(): void
+  {
+    const closeButton = this.elRef.nativeElement.querySelector('.close-button');
+    if (closeButton)
+    {
+      this.closeButtonListenerFn = this.renderer.listen(closeButton,'click', () =>
+      {
+        this.closeModal();
+      });
+    }
+  }
+
+}
+
+class Register
+{
+  name: string;
+  password: string;
+  email: string;
+  roles: string;
+
+  constructor()
+  {
+    this.name = '';
+    this.email = '';
+    this.password = '';
+    this.roles = 'ROLE_USER';
+  }
+}
